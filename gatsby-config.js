0093645590df28d87e883639da8a7406629cd9a1
aosante <aosantea@ucenfotec.ac.cr>
2022-04-10 23:50:39 -0600

    initial config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,55 @@
+const config = require('./src/data/config')
+
 module.exports = {
   siteMetadata: {
-    title: `portfolio-site`,
-    siteUrl: `https://www.yourdomain.tld`
+    author: config.author,
+    description: config.defaultDescription,
+    siteUrl: config.url,
+    skillData: config.skillData,
+    title: config.defaultTitle,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        background_color: config.backgroundColor,
+        display: 'minimal-ui',
+        icon: './static/favicon/favicon-512.png',
+        name: config.defaultTitle,
+        short_name: 'starter',
+        start_url: '/',
+        theme_color: config.themeColor,
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    'gatsby-transformer-remark',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'markdown',
+        path: `${__dirname}/content`,
+      },
     },
-    __key: "pages"
-  }]
-};
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
+    },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: config.themeColor,
+        showSpinner: false,
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
---
+++ src/data/config.js
@@ -0,0 +1,97 @@
+// TODO remove empty values
+module.exports = {
+  defaultTitle: 'Andrés Osante',
+  logo: '',
+  author: 'Andrés',
+  url: 'https://andresosante.com',
+  legalName: 'Andrés Osante',
+  defaultDescription: 'I’m Andrés, a developer from Costa Rica.',
+  socialLinks: {
+    github: 'https://github.com/aosante',
+    linkedin: 'https://www.linkedin.com/in/andr%C3%A9s-osante-alfaro-845701ba/',
+    instagram: 'https://www.instagram.com/aosanaa/',
+  },
+  googleAnalyticsID: '',
+  themeColor: '#ff0a78',
+  backgroundColor: '#ff0a78',
+  social: {
+    facebook: 'appId',
+  },
+  address: {
+    city: 'San José',
+    region: 'Santa Ana',
+    country: 'Costa Rica',
+    zipCode: '10901',
+  },
+  contact: {
+    email: 'andres.osante@gmail.com',
+    phone: '+506 8814-5840',
+  },
+  foundingDate: '2020',
+  recaptcha_key: '6LfuIbkZAAAAAOjlJsaTeDsW2EFB3UfyOJcFbTpm',
+  skillData: [
+    {
+      css: 'linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)',
+      height: 250,
+      name: 'htmlIcon',
+    },
+    {
+      css: 'linear-gradient(135deg, #c3cfe2 0%, #c3cfe2 100%)',
+      height: 250,
+      name: 'cssIcon',
+    },
+    {
+      css: 'linear-gradient(135deg, #e0c3fc 0%, #8ec5fc 100%)',
+      height: 250,
+      name: 'jsIcon',
+    },
+    {
+      css: 'linear-gradient(135deg, #f093fb 0%, #f5576c 100%)',
+      height: 250,
+      name: 'graphqlIcon',
+    },
+    {
+      css: 'linear-gradient(135deg, #a82dea 0%, #fed6e3 100%)',
+      height: 250,
+      name: 'reactIcon',
+    },
+    {
+      css: 'linear-gradient(135deg, #c3cfe2 0%, #c1cfe2 100%)',
+      height: 250,
+      name: 'sassIcon',
+    },
+    {
+      css: 'linear-gradient(135deg, #e3c3fc 0%, #8ec5fc 100%)',
+      height: 250,
+      name: 'sqlserverIcon',
+      isDark: true,
+    },
+    {
+      css: 'linear-gradient(135deg, #f091fb 0%, #f5576c 100%)',
+      height: 250,
+      name: 'mongodbIcon',
+      isDark: true,
+    },
+    {
+      css: 'linear-gradient(135deg, #f091fb 0%, #f5572c 100%)',
+      height: 250,
+      name: 'nodeIcon',
+      isDark: true,
+    },
+    {
+      css: 'linear-gradient(135deg, #f291fb 0%, #f1572c 100%)',
+      height: 250,
+      name: 'vueIcon',
+    },
+    {
+      css: 'linear-gradient(135deg, #f591fb 0%, #f1572c 100%)',
+      height: 250,
+      name: 'materialuiIcon',
+    },
+    {
+      css: 'linear-gradient(135deg, #f291fb 0%, #f1592c 100%)',
+      height: 250,
+      name: 'reduxIcon',
+    },
+  ],
+}
