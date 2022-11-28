749fa0347a51868fe95584fb99b1f3c4bee8aac1
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-11-27 22:48:47 -0600

    remove navigation audio feature

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -1,23 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 import { Wrapper } from './styles'
-import tinkSound from 'assets/audio/tink.wav'
-import soundOn from 'assets/icons/sound-on.jpg'
-import soundOff from 'assets/icons/sound-off.jpg'
 import { useThemeToggle } from '../../ThemeToggleContext'
 
 const NavbarLinks = ({ sidebar }) => {
   const [isDarkTheme, toggleTheme] = useThemeToggle()
-  // @todo: remove after implementing theme switching
-  const [mute, setMute] = useState(false)
-  const [audio] = useState(
-    typeof window !== 'undefined' ? new Audio(tinkSound) : null
-  )
-
-  const playAudio = () => {
-    if (!mute) audio.play().catch((error) => console.log(error))
-  }
 
   return (
     <Wrapper sidebar={sidebar}>
@@ -29,20 +17,10 @@ const NavbarLinks = ({ sidebar }) => {
         {isDarkTheme ? '🌞' : '🌜'}
       </span>
       <div>
-        <AnchorLink onMouseEnter={() => playAudio()} href="#about">
-          About
-        </AnchorLink>
-        <AnchorLink onMouseEnter={() => playAudio()} href="#projects">
-          Projects
-        </AnchorLink>
-        <AnchorLink onMouseEnter={() => playAudio()} href="#contact">
-          Contact
-        </AnchorLink>
-        <a
-          onMouseEnter={() => playAudio()}
-          href="andres-osante-resume.pdf"
-          download
-        >
+        <AnchorLink href="#about">About</AnchorLink>
+        <AnchorLink href="#projects">Projects</AnchorLink>
+        <AnchorLink href="#contact">Contact</AnchorLink>
+        <a href="andres-osante-resume.pdf" download>
           Resume
         </a>
       </div>
